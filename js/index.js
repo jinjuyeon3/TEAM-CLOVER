f5b74dfa76e489b4b76aa046ccd8baf6fc5f8c97
연진주 <jinjuyeon3@gmail.com>
2026-03-27 17:35:46 +0900

    메인페이지 디자인 고민해보자

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // 스크롤 감지
-    const elements = document.querySelectorAll('.fade-up');
+    // fade-up + fade-left 모두 선택, 스크롤 감지
+    const elements = document.querySelectorAll('.fade-up, .fade-left');
 
     const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-        entry.target.classList.add('active');
-        }
-    });
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            }
+        });
     });
 
     elements.forEach(el => observer.observe(el));
-
 });
